0bcf2ea475a7aa80a64a4e7b96133720a508f1f1
yangpengflag <38930516+yangpengflag@users.noreply.github.com>
2019-06-14 18:52:04 +0800

    20190614

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './assets/styles/reset.css'
+import '@/assets/styles/border.css'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  router, /* 原本是router: router，但是基于ES6语法，键值相同时可以直接显示一个就可以了 */
+  components: { App }, /* 原本是App: App，但是基于ES6语法，键值相同时可以直接显示一个就可以了 */
+  template: '<App/>',
   render: h => h(App)
-})
+}).$mount('#app')
+
+/* 路由就是根据不同的地址,返回不同的内容给用户 */
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import fastClick from 'fastclick'
+import Home from '@/pages/home/Home.vue'
 
 Vue.use(Router)
+// 部分手机或机型会存在click事件300ms延迟问题，解决办法是引入fastclick
+// 在项目目录下执行：npm install fastclick --save ，在开发和线上环境都可以使用
+fastClick.attach(document.body)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     }
   ]
 })
